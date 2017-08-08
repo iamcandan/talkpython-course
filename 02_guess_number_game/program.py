822f7d98b7a8ca13288355d182a46641db1d78a5
iamcandan <serhatcandan@gmail.com>
2017-08-08 23:42:37 +0300

    first commit

--- 02_guess_number_game/program.py
+++ 02_guess_number_game/program.py
@@ -3,29 +3,32 @@
 # you can use print( variable, type(variable)) to control type
 
 import random
+
 print('--------------------------')
 print('     Guess number Game')
 print('--------------------------')
 print()
 
-
-the_number = random.randint(0, 100)
+the_number = random.randint (0, 100)
 
 guess = -1
 
+name = input ('What is your name Player? ')
+
 while guess != the_number:
 
-    guess_text = input('Guess a number between 0 and 100: ')
+    guess_text = input ('Guess a number between 0 and 100: ')
 
-    guess = int(guess_text)
+    guess = int (guess_text)
 
     if guess < the_number:
-        print("too low")
+        print('Sorry {}, your guess of {} was too LOW'.format (name, guess))
 
     elif guess > the_number:
-        print('too high')
+        print('Sorry {}, your guess of {} was too HIGH'.format (name, guess))
+
 
     else:
-        print("you win!")
+        print('Excellent work {}, you won, it was {}'.format (name, guess))
 
 print('done')
